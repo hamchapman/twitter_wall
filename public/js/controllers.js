c110bfd4e50d00c1ad111e3183c8e8a33c516178
Hamilton Chapman <hamchapman@gmail.com>
2014-03-26 11:14:04 +0000

    Tidied up unused code and settings view

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,50 +5,14 @@ var twitterWallControllers = angular.module('twitterWallControllers', ['twitterW
 twitterWallControllers.controller('AdminCtrl', [
   '$scope', 
   '$http', 
-  'Pusher', 
-  'CleanTweets', 
-  function ($scope, $http, Pusher, CleanTweets) {
-    $scope.queries = [];
-    $scope.tweets = [];
-    $scope.activeIndexLeft = -1;
-    $scope.activeIndexRight = -1;
-
-    Pusher.subscribe('twitter_wall', 'new_tweet', function (data) {
-      $scope.tweets.push(data['tweet']);
-    });
-
+  function ($scope, $http) {
     $http.get('/api/client-setups')
       .success(function(res) {
         $http.get('/api/stream-restart')
           .success(function(res) {
-            console.log(res);
             console.log("Pre-existing streams restarted");
           });
       });
-    
-    $scope.getTweets = function() {
-      var query = $scope.query;
-      $scope.query = '';
-      $http.get('/api/tweets/' + query)
-    };
-
-    $scope.verifyTweet = function (tweet, $index) {
-      $scope.activeIndexLeft = $index;
-      var i = $scope.tweets.indexOf(tweet);
-      if(i != -1) {
-        $scope.tweets.splice(i, 1);
-      }
-      CleanTweets.add(tweet);
-    };
-
-    $scope.discardTweet = function(tweet, $index) {
-      $scope.activeIndexRight = $index;
-      var i = $scope.tweets.indexOf(tweet);
-      if(i != -1) {
-        $scope.tweets.splice(i, 1);
-      }
-      $http.post('/api/remove-tweet', { tweet: tweet });
-    };
   }
 ]);
 
@@ -66,43 +30,6 @@ twitterWallControllers.controller('ViewerCtrl', [
     Pusher.subscribe('twitter_wall', 'new_clean_tweet', function (data) {
       $scope.cleanTweets.push(data['tweet']);
     });
-  }
-]);
-
-twitterWallControllers.controller('SettingsCtrl', [
-  '$scope', 
-  '$http', 
-  'Pusher',
-  function ($scope, $http, Pusher) {
-    $scope.queries = [];
-    $http.get('/api/queries')
-      .success(function(res) {
-        res.queries.forEach(function(query) {
-          $scope.queries.push(query.text);
-        }) 
-      })
-
-    Pusher.subscribe('twitter_wall', 'new_query', function (data) {
-      $scope.queries.push(data['query']);
-    });
-
-    $scope.updatePusherConfig = function() {
-      var app_id = $scope.pusher_app_id;
-      var key = $scope.pusher_key;
-      var secret = $scope.pusher_secret;
-      var config = { app_id: app_id, key: key, secret: secret };
-      $http.post('/api/pusher', { config: config });
-    }; 
-
-    $scope.removeQuery = function(query, index) {
-      console.log("The query is: " + query);
-      $http.post('/api/remove-query', { index: index, query: query });
-      var i = $scope.queries.indexOf(query);
-      if(i != -1) {
-        $scope.queries.splice(i, 1);
-      }
-    }
-
   }
 ]);
 
